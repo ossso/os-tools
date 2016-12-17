17e1ca48e7ba6414ebd0038b0e9face836b57d04
橙色阳光 <1122@hizhou.cn>
2016-12-17 20:30:21 +0800

    完成等比例高宽缩放

--- tool-script/zoom.js
+++ tool-script/zoom.js
@@ -15,11 +15,20 @@ var Class = function(options,jquery) {
 };
 Class.prototype.init = function() {
     var _this = this
-    this.dom.n10.on('click',function() {
-        _this.transform('16210');
+    this.dom.zoomBtn.on('click',function() {
+        _this.calculate();
     });
-    this.dom.n16.on('click',function() {
-        _this.transform('10216');
+    this.dom.width.on('keypress',function() {
+        if (!_this.dom.lock.get(0).checked) return;
+        setTimeout(function() {
+            _this.watch_keypress('width');
+        },100);
+    });
+    this.dom.height.on('keypress',function() {
+        if (!_this.dom.lock.get(0).checked) return;
+        setTimeout(function() {
+            _this.watch_keypress('height');
+        },100);
     });
     return this;
 };
@@ -28,6 +37,41 @@ Class.prototype.tips = function(msg) {
         layer.msg(msg);
     } catch (e) {
         alert(msg);
+    }
+    return this;
+};
+Class.prototype.calculate = function() {
+    var scale = this.dom.scale.val();
+    var reg = /[^0-9\.]/;
+    if (reg.test(scale) || !scale.length) {
+        this.tips('请输入正确的比例');
+        return this;
+    }
+    var width = this.dom.width.val();
+    var height = this.dom.height.val();
+    if (reg.test(width) || !width.length) {
+        this.tips('请输入正确的宽度数值');
+        return this;
+    } else if (reg.test(height) || !height.length) {
+        this.tips('请输入正确的高度数值');
+        return this;
+    }
+    this.dom.width.val(width*scale);
+    this.dom.height.val(height*scale);
+    return this;
+};
+Class.prototype.watch_keypress = function(type) {
+    if (!this.dom.lock.get(0).checked) return this;
+    if (type == 'width') {
+        var width = this.dom.width.val();
+        var scale = width/this.data.lock_width;
+        this.dom.height.val(this.data.lock_height*scale);
+        this.dom.scale.val(scale);
+    } else {
+        var height = this.dom.height.val();
+        var scale = height/this.data.lock_height;
+        this.dom.width.val(this.data.lock_width*scale);
+        this.dom.scale.val(scale);
     }
     return this;
 };
